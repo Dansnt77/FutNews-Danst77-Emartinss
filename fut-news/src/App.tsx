884f9aa483a8e6828a511bff535473a428b8ba32
emartinss <edumartinscmp@gmail.com>
2023-04-25 20:48:17 -0300

    start project

--- fut-news/src/App.tsx
+++ fut-news/src/App.tsx
@@ -1,11 +1,13 @@
+import { Header } from "./Components/Header/header"
+import { GlobalStyles } from "./styles/global"
 
 function App() {
- 
 
   return (
     <>
-     
-       
+    <GlobalStyles/>
+      <Header />
+
     </>
   )
 }
---
+++ fut-news/src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: none;
+    list-style: none;
+    text-decoration: none;
+    font-family: 'Inter', sans-serif;
+    
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+  dialog{
+    display: unset;
+    position: unset;
+  }
+
+`;
