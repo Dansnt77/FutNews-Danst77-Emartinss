bbb457c4ca655027ddc477c4dc024a51e9d3ea44
emartinss <edumartinscmp@gmail.com>
2023-04-25 20:57:30 -0300

    start project

--- fut-news/src/providers/FutContext.tsx
+++ fut-news/src/providers/FutContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect } from "react";
-import { api } from "../Components/services/api";
+import { api } from "../services/api";
 
 export const FutContext = createContext({});
 
